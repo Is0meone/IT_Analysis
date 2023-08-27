f36a15143874930ca61c0494aa15eb22297171c1
stkwi <stkwiat22a@gmail.com>
2023-08-28 01:50:31 +0200

    small mods

--- JustITScrapper.py
+++ JustITScrapper.py
@@ -8,16 +8,11 @@ def getHTML(URL):
     page_source = driver.page_source
     driver.quit()
     return page_source
-def extractLinks(URL):
+def extractLinks(URL):     #TODO: przewijanie strony ciężka rzecz
     soup = BeautifulSoup(getHTML(URL), "html.parser")
-    # Znalezienie wszystkich linków o klasie "jss840 jss1033"
     resultsBox = soup.find("div",class_="css-110u7ph")
     results = resultsBox.find_all("a")
-
-    # Wyświetlenie liczby znalezionych linków
-    #print(resultsBox.prettify())
     linkArray = []
-    # Przetworzenie znalezionych linków
     for index in range(3, len(results)):
         link = results[index]
         href = link.get("href")
@@ -28,20 +23,19 @@ def extractLinks(URL):
     return linkArray
 def getInfo(link):
     soup = BeautifulSoup(getHTML(link), "html.parser")
+    position = soup.find("div",class_="css-1id4k1").text
     resultsBox = soup.find("div",class_="css-1ikoimk")
-    print(resultsBox.text)
     technologyBox = resultsBox.find_all("div", class_="css-1q98d5e")
     for field in technologyBox:
         tech = field.find("div", class_="css-1eroaug").text
         level = field.find("div", class_="css-19mz16e").text
-        print(tech + " " + level)
+        print(position+" "+ tech + " " + level)
 
 
-URL = "https://justjoin.it/?tab=with-salary"
+URL = "https://justjoin.it"
 linkBox = extractLinks(URL)
 
 for link in linkBox:
     print(link)
     getInfo(link)
     break
-
