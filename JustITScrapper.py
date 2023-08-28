d9394f8f95f09895738bb44f2794b12356d49665
stkwi <stkwiat22a@gmail.com>
2023-08-28 16:13:18 +0200

    scrapped aditional data such as salary, company and make it redable

--- JustITScrapper.py
+++ JustITScrapper.py
@@ -8,7 +8,15 @@ def getHTML(URL):
     page_source = driver.page_source
     driver.quit()
     return page_source
-def extractLinks(URL):     #TODO: przewijanie strony ciężka rzecz
+def cleanSalary(salary):
+    if salary =="Undisclosed Salary - Permanent":
+        return "-"
+    else:
+        salaryParts = salary.split(' ')
+        firstPart = ''.join(salaryParts[:2])
+        secondPart = ''.join(salaryParts[3:5])
+        return firstPart + " " + secondPart
+def extractLinks(URL):
     soup = BeautifulSoup(getHTML(URL), "html.parser")
     resultsBox = soup.find("div",class_="css-110u7ph")
     results = resultsBox.find_all("a")
@@ -22,20 +30,42 @@ def extractLinks(URL):     #TODO: przewijanie strony ciężka rzecz
     print(linkArray)
     return linkArray
 def getInfo(link):
+    jobInfo = []
+
     soup = BeautifulSoup(getHTML(link), "html.parser")
+    print(soup.prettify())
     position = soup.find("div",class_="css-1id4k1").text
+    company = soup.find("a",class_="css-l4opor").text
+    experience = soup.find_all("div",class_="css-1ji7bvd")[1].text
+    salaryInfo = soup.find("div",class_="css-1wla3xl").text
+    print(salaryInfo)
+    salary = cleanSalary(salaryInfo)
+
+    jobInfo.append(position)
+    jobInfo.append(company)
+    jobInfo.append(experience)
+    jobInfo.append(salary)
+
+    techDictionary = {}
     resultsBox = soup.find("div",class_="css-1ikoimk")
     technologyBox = resultsBox.find_all("div", class_="css-1q98d5e")
     for field in technologyBox:
         tech = field.find("div", class_="css-1eroaug").text
         level = field.find("div", class_="css-19mz16e").text
-        print(position+" "+ tech + " " + level)
-
+        techDictionary[tech] = level
+    jobInfo.append(techDictionary)
+    jobInfo.append('-')
+    return jobInfo
 
 URL = "https://justjoin.it"
 linkBox = extractLinks(URL)
 
+scrappedInfo =[]
 for link in linkBox:
     print(link)
-    getInfo(link)
+    scrappedInfo.append(getInfo(link))
     break
+print(scrappedInfo)
+
+# TODO: przewijanie strony
+#       ustandaryzowac writter
